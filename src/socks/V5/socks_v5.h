4b87a87ab244861f434a1f87e7745f5a4e33478c
ZhiyangLeeCN <zhiyangleeCN@gmail.com>
2018-01-10 22:57:12 +0800

    新增SOCKSV5协议

--- src/socks/V5/socks_v5.h
+++ src/socks/V5/socks_v5.h
@@ -3,6 +3,44 @@
 
 #include <winsock2.h>
 
+#define SVERSION 0x05
+#define CONNECT 0x01
+#define IPV4 0x01
+#define DOMAIN 0x03
+#define IPV6 0x04
+#define METHOD_NOAUTH 0x00
+#define METHOD_UNACCEPTABLE 0xff
+#define CMD_NOT_SUPPORTED 0x07
+
+#pragma pack(1)
+
+typedef struct _METHOD_SELECT_REQUEST {
+	unsigned char ver;
+	unsigned char nmethods;
+	unsigned char methods[255];
+} METHOD_SELECT_REQUEST;
+
+typedef struct _METHOD_SELECT_RESPONSE {
+	unsigned char ver;
+	unsigned char method;
+} METHOD_SELECT_RESPONSE;
+
+typedef struct _SOCKS5_REQUEST {
+	unsigned char ver;
+	unsigned char cmd;
+	unsigned char rsv;
+	unsigned char atyp;
+} SOCKS5_REQUEST;
+
+typedef struct _SOCKS5_RESPONSE {
+	unsigned char ver;
+	unsigned char rep;
+	unsigned char rsv;
+	unsigned char atyp;
+} SOCKS5_RESPONSE;
+
+#pragma pack()
+
 int ConnectSocksV5Server(
 		SOCKET   s,
 		SOCKADDR *socksServerAddr,
